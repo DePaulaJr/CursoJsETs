8563d33905856e181b11fbee410fe5da35ff4611
DePaulaJr <chicomzb@gmail.com>
2021-03-28 01:21:58 -0300

    Create retrospectiva.js

---
+++ aula8/retrospectiva.js
@@ -0,0 +1,10 @@
+//String, number, undefined, null, boolean, symbol
+const nome = 'Luiz'; //string
+const nome1 = "Luiz";//string
+const nome2 = `Luiz`;//string - template strings
+const num1 = 10;//number
+const num2 = 10.52;//number
+let nomeAluno;//undefined - não aponta para nenhum local (nunca definir undefined. Deixar para o sistema.)
+const sobrenomeAluno = null;//Nulo -  não aponta para nenhum local
+const aprovado = false;//Boolean - true, false (lógico)
+
