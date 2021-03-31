6eaba6fb72636b9ad8396ac55cc464f7f6520ece
DePaulaJr <chicomzb@gmail.com>
2021-03-30 23:07:16 -0300

    Izi

---
+++ aula15/index.js
@@ -0,0 +1,16 @@
+//console.log(!!(100 / 0));True
+
+//let num1 = 9;
+//let num2 = Math.floor(num1);arredonda pra baixo
+//let num2 = Math.ceil(num1);arredonda pra cima
+//let num2 = Math.round(num1);arredonda pro mais próximo
+//console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6));maior número da sequência
+//console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6));menor número da sequência
+//console.log(Math.random());número aleatório entre zero e um
+//const aleatorio = Math.round(Math.random() * (10 - 5) +5);numero aleatório entre 5 e 10 e arredondado pra inteiro
+//console.log(aleatorio);
+
+//console.log(Math.pow(2, 10));potenciação
+//console.log(2 ** 10);potenciação
+//console.log(num1 ** (1/2));raíz quadrada
+//console.log(num1 **0.5);raíz quadrada
