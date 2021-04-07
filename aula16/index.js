e38db3e1dd790e1bd982e07f727a389e535d387b
DePaulaJr <chicomzb@gmail.com>
2021-04-07 02:57:33 -0300

    Create index.js

---
+++ aula16/index.js
@@ -0,0 +1,26 @@
+//               0        1       2
+const alunos = ['Luiz', 'Maria', 'João'];
+
+
+
+console.log(alunos);
+
+
+//alunos.push('Luiza');//Adiciona no fim
+//alunos.unshift('Luiza');//Adiciona no zero
+//alunos.unshift('Fábio');//Adiciona no zero depois do primeiro adicionado no zero
+//alunos[0] = 'Eduardo';//Alterado
+//alunos[3] = 'Marcos';//Criado
+//alunos[alunos.length] = 'Luiza';//Adiciona no fim
+//alunos[alunos.length] = 'Fábio';
+//alunos[alunos.length] = 'Luana';
+//alunos.push('Ronaldo');//Adiciona no fim
+//alunos.push('Sebastião');
+//const removido = alunos.pop();//Remove o último item
+//const removido = alunos.shift();//Remove o primeiro item
+//console.log(removido);
+//delete alunos[1];//Deleta a informação na parter da memoria escolhida e deixa vazia
+//console.log(alunos.slice(0,2));//Corta o array e exibe. Primeiro número inicia e segundo corta até o número anterior
+//console.log(alunos.slice(0, -2))//Faz o mesmo porém do último pro primeiro
+//console.log(typeof alunos);//Array é object
+//console.log(alunos instanceof Array);//Resultado booleano
