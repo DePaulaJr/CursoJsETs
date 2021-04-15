d2132bf68bac7101d4d7b8003a61312685e21ab8
DePaulaJr <chicomzb@gmail.com>
2021-04-15 04:22:45 -0300

    Create index.js

---
+++ aula19/index.js
@@ -0,0 +1,5 @@
+/*
+Primitivos (imutáveis) - string, number, boolean, undefined, null (bigint, symbol) - valores copiados
+
+Referência (Mutável) - array, object, function - Passados por referência
+*/
