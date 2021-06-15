c9b0191021f6afca1ddabeb7e69d71176eef7c73
DePaulaJr <chicomzb@gmail.com>
2021-06-15 13:07:11 -0300

    Create index.js

---
+++ aula22/index.js
@@ -0,0 +1,12 @@
+/*
+Operadores Lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+││ -> OR -> OU
+! -> NOT -> NÃO
+*/
+
+//const expressaoAnd = true && true && true && true;
+//console.log(expressaoAnd);
+//const expressaoOr = true || false || false || false; 
+//console.log(expressaoOr);
+//console.log(!false);
